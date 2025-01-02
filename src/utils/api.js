b935adceac02f745474750c867836e27716fd38c
KARANSB123 <sbkaran142@gmail.com>
2025-01-02 13:36:50 +0530

    first

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: 'https://backend-project-guvi-final.onrender.com/api', 
   headers: {
     'Content-Type': 'application/json',
   },
